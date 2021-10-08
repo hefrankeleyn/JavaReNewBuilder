36e0b53cfa621278524635bdae59088c2669bd1f
Li Fei <hefrankeleyn@gamil.com>
2021-10-08 21:40:16 +0800

    堆排序

---
+++ projects/pro03Algorithm/src/main/java/com/hef/stack/HeapSort.java
@@ -0,0 +1,49 @@
+package com.hef.stack;
+
+import java.util.Arrays;
+
+/**
+ * @Date 2021/10/8
+ * @Author lifei
+ */
+public class HeapSort {
+
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int k=N/2; k>0; k--) {
+            sink(a, k, N);
+        }
+        while (N>=1) {
+            exch(a, 1, N--);
+            sink(a, 1, N);
+        }
+    }
+
+    private static void sink(Comparable[] a, int k, int N) {
+        while (2 * k <= N) {
+            int j = 2 * k;
+            if (j<N && less(a, j+1, j)) j++;
+            if (less(a, k, j)) break;
+            exch(a, k, j);
+            k = j;
+        }
+    }
+
+
+    private static boolean less(Comparable[] a, int i, int j) {
+        return a[i-1].compareTo(a[j-1])<0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i-1];
+        a[i-1] = a[j-1];
+        a[j-1] = t;
+    }
+
+    public static void main(String[] args) {
+        Integer[] a  = {9, 0, 4, 5, 2, 1, 9, 0, 4, 5, 2, 1, 0, 4, 5, 2, 1, 9, 0};
+        System.out.println(Arrays.toString(a));
+        sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
