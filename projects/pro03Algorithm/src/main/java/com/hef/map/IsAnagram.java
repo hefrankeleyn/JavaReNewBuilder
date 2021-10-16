bf2ed26033e278da791b47e821a2fa084d040fcc
Li Fei <hefrankeleyn@gamil.com>
2021-10-16 11:32:59 +0800

    hashMap 相关题目

---
+++ projects/pro03Algorithm/src/main/java/com/hef/map/IsAnagram.java
@@ -0,0 +1,21 @@
+package com.hef.map;
+
+import java.util.*;
+
+/**
+ * @Date 2021/10/16
+ * @Author lifei
+ */
+public class IsAnagram {
+
+    public static void main(String[] args) {
+        char[] a ={'a','b'};
+        char[] b ={'a','b'};
+        Arrays.equals(a, b);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
+
+        }
+
+    }
+}
