c8e368ebb40e03d4cc9e41eb718f9bc5b4e4ca05
Li Fei <hefrankeleyn@gamil.com>
2021-11-09 22:19:59 +0800

    每日在线用户
    每日在线用户求解

---
+++ projects/pro03Algorithm/src/main/java/com/hef/demo/DailyTemperatures.java
@@ -0,0 +1,30 @@
+package com.hef.demo;
+
+import java.util.*;
+/**
+ * @Date 2021/11/9
+ * @Author lifei
+ */
+public class DailyTemperatures {
+
+    public static void main(String[] args) {
+        DailyTemperatures dailyTemperatures = new DailyTemperatures();
+        int[] T = {73,74,75,71,69,72,76,73};
+        int[] res = dailyTemperatures.dailyTemperatures(T);
+        System.out.println(Arrays.toString(res));
+    }
+
+    public int[] dailyTemperatures(int[] T) {
+        int n = T.length;
+        int[] res = new int[n];
+        Deque<Integer> deque = new LinkedList<>();
+        for (int i=0; i<n; i++) {
+            while (!deque.isEmpty() && T[deque.peek()]<T[i]) {
+                int j = deque.pop();
+                res[j] = i-j;
+            }
+            deque.push(i);
+        }
+        return res;
+    }
+}
